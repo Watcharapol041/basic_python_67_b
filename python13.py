b1ec78cf61b1a7bf76325f65b3913e6f3fe5a3d0
Watcharapol041 <boompolrob7@gmail.com>
2024-08-25 16:00:16 +0700

    datetime, msath, json, try

---
+++ python13.py
@@ -0,0 +1,8 @@
+"""
+#
+# Part: Python Input
+#
+"""
+username = Input("Enter your username:")
+password = Input("Enter your password:")
+print(username, password)
---
+++ python14.py
@@ -0,0 +1,13 @@
+"""
+#
+# Part: PIP (Python Install Package)
+#
+"""
+import camelcase
+camel = camelcase.CamelCase()
+txt = "hello world my friend"
+print(camel.hump(txt))
+
+# pip list
+# pip install comelcase
+# pip uninstall camelcase
